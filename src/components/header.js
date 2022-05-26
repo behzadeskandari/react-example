0d578a9dc49633c47964aa6e03f5495dc1532542
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2022-05-26 16:46:21 +0430

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+        <header>
+            <div className="logo">Logo</div>
+            <input />
+        </header>
+    )
+  }
+}
