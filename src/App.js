f090f87fa45eab5e3f40e0f557df77a26f03b341
behzad.b.i.g@gmail.com <behza@DESKTOP-62KEJTU>
2022-05-26 18:02:13 +0430

    Practice WebSite Component Built

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import React from 'react';
 import logo from './logo.svg';
+import JSONSecond from './db.json';
 import './App.css';
 import Header from './components/header';
+import NewsList from './components/news_list';
+import Footer from './components/footer';
 
-function App() {
-  return (
-    <div>
-      <>
-        <Header/>
-      </>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    news: JSONSecond,
+    footerText: 'I am Happy Footer'
+  }
+
+  render(){
+
+    const {news ,footerText} = this.state;
+    console.log(this.state.news)
+    return (
+      <div>
+        <>
+          <Header />
+          <NewsList news={news}>
+              <h1>Header</h1>
+
+          </NewsList>
+          <Footer footerText={footerText}/>
+        </>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react'
 import './header.css';
 
 export default class Header extends Component {
+    state = {
+        name : 'behzad',
+        title: 'programmer',
+        keyword: '',
+        count : 0,
+    }
+
+
+  inputChangeHander = (event) => {
+        console.log(event.target.value)
+        console.log('hey')
+        this.setState({keyword : event.target.value});
+  }
+  addOne(){
+    //   this.setState(this.state.count + 1)
+    this.setState((state, props)=> ({
+        count : state.count + 1
+    }));
+}
   render() {
     return (
         <header>
             <div className="logo">Logo</div>
-            <input />
+            <input onChange={this.inputChangeHander} />
+            {/* <div>{this.state.keyword}</div>
+            <div>{this.state.count}</div>
+            <button onClick={() => this.addOne()}></button> */}
         </header>
+
     )
   }
 }
